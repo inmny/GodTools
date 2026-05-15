cb2448eacd3f393ee963fa9f3641d6ad8a08de3a
inmny <1342885270@qq.com>
2026-05-15 17:56:03 +0800

    修复隐藏鼠标、掉落物相关的bug

--- Code/Patch/PatchAboutHideDrop.cs
+++ Code/Patch/PatchAboutHideDrop.cs
@@ -28,13 +28,15 @@ namespace GodTools.Patch
             var list = instructions.ToList();
             var index = list.FindIndex(x => (x.opcode == OpCodes.Call) && (x.operand as MethodInfo).Name == "get_world");
             var label = list[index].labels[0];
+            var new_label = new Label();
             var old_code = list[index];
             list.InsertRange(index, new[] { 
                 new CodeInstruction(OpCodes.Ldsfld, AccessTools.Field(typeof(CustomGodToolsConfig), nameof(CustomGodToolsConfig.HideDrop))), 
-                new CodeInstruction(OpCodes.Brfalse_S, label),
+                new CodeInstruction(OpCodes.Brfalse_S, new_label),
                 new CodeInstruction(OpCodes.Ret)
             });
             old_code.MoveLabelsTo(list[index]);
+            old_code.labels.Add(new_label);
             return list;
         }
         [HarmonyPrefix, HarmonyPatch(typeof(DropsLibrary), nameof(DropsLibrary.flash))]
