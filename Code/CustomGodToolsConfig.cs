3bef0a3ccdb1aea55a59b94e28173a7b90d05345
inmny <1342885270@qq.com>
2026-05-20 10:39:21 +0800

    feat: 添加黑夜纪元遮罩透明度调节功能

--- Code/CustomGodToolsConfig.cs
+++ Code/CustomGodToolsConfig.cs
@@ -1,12 +1,25 @@
+using NeoModLoader.api.attributes;
+using UnityEngine;
+
 namespace GodTools;
 
 public static class CustomGodToolsConfig
 {
     public static float AgeLightChangeSpeed { get; private set; } = 1f;
+    public static float DarkAgeOverlayAlpha { get; private set; } = 0.3f;
 
     public static void SetAgeLightChangeSpeed(float value)
     {
         AgeLightChangeSpeed = value < 0f ? 0f : value;
+    }
+    [Hotfixable]
+    public static void SetDarkAgeOverlayAlpha(float value)
+    {
+        DarkAgeOverlayAlpha = Mathf.Clamp01(value);
+
+        var dark_age = AssetManager.era_library.get("age_dark");
+        if (dark_age != null) dark_age.era_effect_overlay_alpha = DarkAgeOverlayAlpha;
+        Main.LogInfo(PlayerConfig.getIntValue("age_overlay_effect").ToString());
     }
 
     public static bool HideCursor { get; private set; }
